c8b099683cb88eff46d9f2c35804161afc41b142
xBis7 <christosbis@gmail.com>
2024-02-08 18:29:46 +0200

    run_all.sh parameters fixed

--- trino-hms-hdfs-ranger/run_all.sh
+++ trino-hms-hdfs-ranger/run_all.sh
@@ -3,12 +3,18 @@
 source "./testlib.sh"
 
 abs_path=$1
+github_user=$2
+github_remote_user=$3
+build_starting_step=$4
+java_8_home=$5
+java_21_home=$6
+
 
 # Clone projects or just pull changes if projects exist.
-./get_or_update_projects.sh "$abs_path"
+./get_or_update_projects.sh "$abs_path" "$github_user" "$github_remote_user"
 
 # Build projects.
-./build_projects.sh "$abs_path"
+./build_projects.sh "$abs_path" "$build_starting_step" "$java_8_home" "$java_21_home"
 
 # Copy jars and config files.
 ./setup_docker_env.sh "$abs_path"

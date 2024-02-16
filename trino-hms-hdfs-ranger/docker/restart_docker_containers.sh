c087bce03d2819a1087eaba5310c99211f9cc205
xBis7 <christosbis@gmail.com>
2024-02-16 21:11:03 +0200

    hive-site.xml cleanup

--- trino-hms-hdfs-ranger/docker/restart_docker_containers.sh
+++ trino-hms-hdfs-ranger/docker/restart_docker_containers.sh
@@ -3,5 +3,5 @@
 abs_path=$1
 project=$2
 
-./docker/stop_docker_containers "$abs_path" "$project"
-./docker/start_docker_containers "$abs_path" "$project"
+./docker/stop_docker_containers.sh "$abs_path" "$project"
+./docker/start_docker_containers.sh "$abs_path" "$project"

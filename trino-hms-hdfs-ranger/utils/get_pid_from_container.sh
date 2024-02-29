4cab43ef068c1acd4b6a78673fb88447ab303cbb
xBis7 <christosbis@gmail.com>
2024-02-29 17:02:26 +0200

    fix get_pid_from_container

--- trino-hms-hdfs-ranger/utils/get_pid_from_container.sh
+++ trino-hms-hdfs-ranger/utils/get_pid_from_container.sh
@@ -5,7 +5,13 @@ source "./testlib.sh"
 set -e
 
 container_name=$1
+use_jps=$2
 
 hostname=$(getHostnameFromName "$container_name")
 
-docker exec -it "$hostname" jps
+if [ "$use_jps" == "false" ]; then
+  docker exec -it "$hostname" bash -c "ps aux | grep java"
+else
+  docker exec -it "$hostname" jps
+fi
+

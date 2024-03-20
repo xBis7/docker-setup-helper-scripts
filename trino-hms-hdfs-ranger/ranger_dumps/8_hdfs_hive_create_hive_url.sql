df286720dae3f5ebc926e0fb3da75b0e1c39f3d1
mladjan-gadzic <gadzic.mladjan@gmail.com>
2024-03-20 12:37:24 +0100

    Fix PG dumps docs

--- trino-hms-hdfs-ranger/ranger_dumps/8_hdfs_hive_create_hive_url.sql
+++ trino-hms-hdfs-ranger/ranger_dumps/8_hdfs_hive_create_hive_url.sql
@@ -1,12 +1,8 @@
--- Service Name     | Policy Name                                   | Groups            | Users                             | Permissions
---------------------|-----------------------------------------------|-------------------|-----------------------------------|---------------------
--- hadoopdev        | all-path                                      | -                 | hadoop, postgres, spark           | Read, Write, Execute
--- hivedev          | all - database                                | public            | -                                 | Create
--- hivedev          | all - database                                | -                 | hive                              | *
--- hivedev          | all - url                                     | -                 | spark, postgres                   | Write
--- hivedev          | all - *                                       | -                 | hive                              | *
--- hivedev          | default database tables column                | -                 | postgres, spark                   | *
--- hivedev          | Information_schema database tables columns    | public            | -                                 | select
+-- Service Name     | Policy Name                                   | Users                             | Permissions
+--------------------|-----------------------------------------------|-----------------------------------|-------------
+-- hadoopdev        | all-path                                      | hadoop, postgres, spark           | Read, Write, Execute
+-- hivedev          | all - url                                     | spark, postgres                   | write
+-- hivedev          | default database tables column                | postgres, spark                   | *
 
 --
 -- PostgreSQL database dump

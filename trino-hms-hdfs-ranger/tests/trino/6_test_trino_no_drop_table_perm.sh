e6a0954c8a9a2965009045d345790e738ae66e35
Mladjan Gadzic <gadzic.mladjan@gmail.com>
2024-03-28 16:24:11 +0100

    Fix commented out test (#17)
    Fix commented out test

--- trino-hms-hdfs-ranger/tests/trino/6_test_trino_no_drop_table_perm.sh
+++ trino-hms-hdfs-ranger/tests/trino/6_test_trino_no_drop_table_perm.sh
@@ -12,8 +12,9 @@ echo "- INFO: Reusing policies from previous test"
 echo ""
 echo "- INFO: [drop] should fail."
 
-# We would expect this test to fail with the 'failMsg' but it fails with a diffrent error.
-# 'Query 20240308_161853_00006_6nb76 failed: The following metastore delete operations failed: drop table default.new_trino_test_table'
-failMsg="Permission denied: user [postgres] does not have [DROP] privilege on [default/$NEW_TRINO_TABLE_NAME]"
-
+# It is expected for this test to fail with the permission denied exception with message similar to 'Permission denied: user [postgres] does not have [DROP] privilege on [default/new_trino_test_table]', but it fails with a different error.
+# Thanks to xBis7 for investigation, it was found that permission denied exception is swallowed and a new exception is thrown with a different message.
+# This seems to done for better messaging experience, but it is misleading.
+# More info could be found on the link https://github.com/G-Research/gr-oss/issues/551#issuecomment-1994240651.
+failMsg="The following metastore delete operations failed: drop table default.$NEW_TRINO_TABLE_NAME"
 retryOperationIfNeeded "$abs_path" "dropTrinoTable $NEW_TRINO_TABLE_NAME" "$failMsg" "true"

62230cda7f85a44685cf9d80a0d5ff053295fb19
xBis7 <christosbis@gmail.com>
2024-04-08 21:15:45 +0300

    update for hive version 4.0.0

--- trino-hms-hdfs-ranger/setup/setup_docker_env.sh
+++ trino-hms-hdfs-ranger/setup/setup_docker_env.sh
@@ -22,7 +22,7 @@ hdfs_jars_path="$abs_path/$PROJECT_HADOOP/hadoop-dist/target/hadoop-3.3.6/share/
 hdfs_conf_path="$abs_path/$PROJECT_HADOOP/hadoop-dist/target/hadoop-3.3.6/etc/hadoop"
 
 # Hive path
-hive_jars_path="$abs_path/$PROJECT_HIVE/packaging/target/apache-hive-3.1.5-bin/apache-hive-3.1.5-bin/lib"
+hive_jars_path="$abs_path/$PROJECT_HIVE/packaging/target/apache-hive-4.0.0-bin/apache-hive-4.0.0-bin/lib"
 
 # Ranger - HDFS setup
 echo "Copying Ranger jars under HDFS."
@@ -45,7 +45,7 @@ execCmdAndHandleErrorIfNeeded "cp $ranger_hive_jar_path $hive_jars_path"
 echo "Copy finished."
 
 echo "Making hive 'entrypoint.sh' executable."
-hive_generated_entrypoint_path="$abs_path/$PROJECT_HIVE/packaging/target/apache-hive-3.1.5-bin/apache-hive-3.1.5-bin/compose/hive-metastore-ranger"
+hive_generated_entrypoint_path="$abs_path/$PROJECT_HIVE/packaging/target/apache-hive-4.0.0-bin/apache-hive-4.0.0-bin/compose/hive-metastore-ranger"
 execCmdAndHandleErrorIfNeeded "chmod u+x $hive_generated_entrypoint_path/entrypoint.sh"
 echo "Permissions updated."
 
--- trino-hms-hdfs-ranger/testlib.sh
+++ trino-hms-hdfs-ranger/testlib.sh
@@ -85,28 +85,28 @@ RANGER_HDFS_JAR="hdfs-agent/target/$RANGER_HDFS_JAR_NAME"
 RANGER_HIVE_JAR="hive-agent/target/$RANGER_HIVE_JAR_NAME"
 
 # Hive jars names
-HIVE_BEELINE_JAR_NAME="hive-beeline-3.1.5.jar"
-HIVE_CLI_JAR_NAME="hive-cli-3.1.5.jar"
-HIVE_COMMON_JAR_NAME="hive-common-3.1.5.jar"
-HIVE_EXEC_CORE_JAR_NAME="hive-exec-3.1.5-core.jar"
-HIVE_EXEC_JAR_NAME="hive-exec-3.1.5.jar"
-HIVE_JDBC_STANDALONE_JAR_NAME="hive-jdbc-3.1.5-standalone.jar"
-HIVE_JDBC_JAR_NAME="hive-jdbc-3.1.5.jar"
-HIVE_LLAP_COMMON_JAR_NAME="hive-llap-common-3.1.5.jar"
-HIVE_METASTORE_JAR_NAME="hive-metastore-3.1.5.jar"
-HIVE_SERDE_JAR_NAME="hive-serde-3.1.5.jar"
-HIVE_SERVICE_RPC_JAR_NAME="hive-service-rpc-3.1.5.jar"
-HIVE_SHIMS_JAR_NAME="hive-shims-3.1.5.jar"
-HIVE_SHIMS_COMMON_JAR_NAME="hive-shims-common-3.1.5.jar"
+HIVE_BEELINE_JAR_NAME="hive-beeline-4.0.0.jar"
+HIVE_CLI_JAR_NAME="hive-cli-4.0.0.jar"
+HIVE_COMMON_JAR_NAME="hive-common-4.0.0.jar"
+HIVE_EXEC_CORE_JAR_NAME="hive-exec-4.0.0-core.jar"
+HIVE_EXEC_JAR_NAME="hive-exec-4.0.0.jar"
+HIVE_JDBC_STANDALONE_JAR_NAME="hive-jdbc-4.0.0-standalone.jar"
+HIVE_JDBC_JAR_NAME="hive-jdbc-4.0.0.jar"
+HIVE_LLAP_COMMON_JAR_NAME="hive-llap-common-4.0.0.jar"
+HIVE_METASTORE_JAR_NAME="hive-metastore-4.0.0.jar"
+HIVE_SERDE_JAR_NAME="hive-serde-4.0.0.jar"
+HIVE_SERVICE_RPC_JAR_NAME="hive-service-rpc-4.0.0.jar"
+HIVE_SHIMS_JAR_NAME="hive-shims-4.0.0.jar"
+HIVE_SHIMS_COMMON_JAR_NAME="hive-shims-common-4.0.0.jar"
 HIVE_SHIMS_SCHEDULER_JAR_NAME="hive-shims-scheduler-3.1.3.jar"
 HIVE_SPARK_CLIENT_JAR_NAME="hive-spark-client-3.1.3.jar"
-HIVE_STANDALONE_METASTORE_SERVER_JAR_NAME="hive-standalone-metastore-server-3.1.5.jar"
-HIVE_STANDALONE_METASTORE_BENCHMARKS_JAR_NAME="hive-metastore-benchmarks-3.1.5.jar"
-HIVE_STANDALONE_METASTORE_TOOLS_COMMON_JAR_NAME="metastore-tools-common-3.1.5.jar"
+HIVE_STANDALONE_METASTORE_SERVER_JAR_NAME="hive-standalone-metastore-server-4.0.0.jar"
+HIVE_STANDALONE_METASTORE_BENCHMARKS_JAR_NAME="hive-metastore-benchmarks-4.0.0.jar"
+HIVE_STANDALONE_METASTORE_TOOLS_COMMON_JAR_NAME="metastore-tools-common-4.0.0.jar"
 
 # We probably don't need those. Don't copy them for now. They are both under 'ql/target'
-HIVE_EXEC_FALLBACKAUTHORIZER_JAR_NAME="hive-exec-3.1.5-fallbackauthorizer.jar"
-HIVE_ORIGINAL_EXEC_JAR_NAME="original-hive-exec-3.1.5.jar"
+HIVE_EXEC_FALLBACKAUTHORIZER_JAR_NAME="hive-exec-4.0.0-fallbackauthorizer.jar"
+HIVE_ORIGINAL_EXEC_JAR_NAME="original-hive-exec-4.0.0.jar"
 
 # Hive jars, paths from Hive project root
 HIVE_BEELINE_JAR="beeline/target/$HIVE_BEELINE_JAR_NAME"
@@ -314,7 +314,7 @@ handleHiveEnv() {
   abs_path=$1
   op=$2
 
-  hive_docker_path="$abs_path/$PROJECT_HIVE/packaging/target/apache-hive-3.1.5-bin/apache-hive-3.1.5-bin/compose/hive-metastore-ranger"
+  hive_docker_path="$abs_path/$PROJECT_HIVE/packaging/target/apache-hive-4.0.0-bin/apache-hive-4.0.0-bin/compose/hive-metastore-ranger"
   cd $hive_docker_path
 
   if [ "$op" == "start" ]; then

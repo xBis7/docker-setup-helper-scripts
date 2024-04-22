063a5a10bb89f5c9856885e2717fd8384fe8e603
Mladjan Gadzic <gadzic.mladjan@gmail.com>
2024-04-22 10:17:46 +0200

    Uncomment test and add info when to comment it

--- trino-hms-hdfs-ranger/tests/trino/5_test_trino_select_alter_perm.sh
+++ trino-hms-hdfs-ranger/tests/trino/5_test_trino_select_alter_perm.sh
@@ -49,12 +49,12 @@ retryOperationIfNeeded "$abs_path" "alterTrinoTable $TRINO_TABLE $NEW_TRINO_TABL
 # successMsg="INSERT: 1 row"
 # retryOperationIfNeeded "$abs_path" "performTrinoCmd $cmd" "$successMsg" "false"
 
-# TODO: Fix this test
+# FIXME: Comment out this test for Hive 4
 # It fails with the following error: "Query 20240402_105548_00016_g47rj failed: Cannot delete from non-managed Hive table".
 # It should not fail because $TABLE_ANIMALS is created in Hive managed space.
-#echo ""
-#echo "- INFO: Delete from $TABLE_ANIMALS table."
-#echo "- INFO: [alter] should succeed."
-#cmd="delete from hive.default.$TABLE_ANIMALS;"
-#successMsg="DELETE"
-#retryOperationIfNeeded "$abs_path" "performTrinoCmd $cmd" "$successMsg" "false"
+echo ""
+echo "- INFO: Delete from $TABLE_ANIMALS table."
+echo "- INFO: [alter] should succeed."
+cmd="delete from hive.default.$TABLE_ANIMALS;"
+successMsg="DELETE"
+retryOperationIfNeeded "$abs_path" "performTrinoCmd $cmd" "$successMsg" "false"

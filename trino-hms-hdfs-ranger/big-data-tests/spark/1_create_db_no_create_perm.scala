fa24b76d3586474309d5f38abbfc202918772e04
Christos Bisias <christosbis@gmail.com>
2024-06-14 18:31:48 +0300

    tests 1,2,3

--- trino-hms-hdfs-ranger/big-data-tests/spark/1_create_db_no_create_perm.scala
+++ trino-hms-hdfs-ranger/big-data-tests/spark/1_create_db_no_create_perm.scala
@@ -1,24 +1,11 @@
+
 val user = spark.conf.get("spark.user", "spark")
-val db_base_dir = spark.conf.get("spark.db_base_dir", "/data/projects")
+val dbBaseDir = spark.conf.get("spark.db_base_dir", "/data/projects")
 
-val db_location = db_base_dir + "/gross_test/gross_test.db"
+val dbName="gross_test"
 
-val expectedErrorMsg = "Permission denied: user [" + user + "] does not have [CREATE] privilege on [gross_test]"
+val dbLocation = dbBaseDir + "/gross_test/gross_test.db"
 
-try {
-  spark.sql("create database gross_test location '" + db_location + "'")
-} catch {
-  case e: Exception =>
-    if (e.getMessage().contains(expectedErrorMsg)) {
-      println("'create database' failed with the expected error.")
-      println("Message: " + e.getMessage())
-      sys.exit(0)
-    } else {
-      println("'create database' failed with the wrong error.")
-      println("Message: " + e.getMessage())
-      sys.exit(1)
-    }
-}
+val expectedErrorMsg = s"Permission denied: user [$user] does not have [CREATE] privilege on [$dbName]"
 
-println("Test finished without issues while it was expected to fail.")
-sys.exit(1)
+CommonUtils.createDBwithException(dbName, dbLocation, expectedErrorMsg)

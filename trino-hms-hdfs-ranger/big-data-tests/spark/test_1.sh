ccc63c996f10caed9955b165c16eface26e8f2a8
Christos Bisias <christosbis@gmail.com>
2024-06-18 20:57:56 +0300

    tests 4-11 simplified

--- trino-hms-hdfs-ranger/big-data-tests/spark/test_1.sh
+++ trino-hms-hdfs-ranger/big-data-tests/spark/test_1.sh
@@ -14,20 +14,16 @@ echo ""
 # 'permissions' have this format -> 'operation1,operation2:user1,user2/operation4,operation5,operation6:user3'
 # e.g. 'read,write:hadoop,spark/read,execute:hive'
 
-# 2nd parameter is 'resources'.
-# 'resources' have this format -> 'resource1,resource2'
-# e.g. '/dir1/*,/dir2/dir3/*'
-
 # 1st parameter: permissions
 # 2nd parameter: comma-separated list of paths
-updateHdfsPathPolicy "read,write,execute:$HDFS_USER,$HIVE_USER,$SPARK_USER1" "/*"
+updateHdfsPathPolicy "read,write,execute:$HDFS_USER,$SPARK_USER1" "/*"
 
 # 1st parameter: permissions
 # 2nd parameter: comma-separated list of DBs
-updateHiveDbAllPolicy "select:$HIVE_USER,$SPARK_USER1"
+updateHiveDbAllPolicy "select:$SPARK_USER1"
 
 # 1st parameter: permissions
-updateHiveDefaultDbPolicy "select:$HIVE_USER,$SPARK_USER1"
+updateHiveDefaultDbPolicy "select:$SPARK_USER1"
 
 # 1st parameter: permissions
 # 2nd parameter: comma-separated list of URLs
@@ -35,13 +31,13 @@ updateHiveUrlPolicy "read:$SPARK_USER1"
 
 waitForPoliciesUpdate
 
-expectedErrorMsg="Permission denied: user [$SPARK_USER1] does not have [CREATE] privilege on [gross_test]"
-
 # 'data/projects' can be replaced by '$EXTERNAL_HIVE_DB_PATH'
 command="spark.sql(\"create database gross_test location '/data/projects/gross_test/gross_test.db'\")"
 
+expectedErrorMsg="Permission denied: user [$SPARK_USER1] does not have [CREATE] privilege on [gross_test]"
+
 # 1st parameter: the user to execute the command
 # 2nd parameter: the command to be executed
-# 3rd parameter: 'true' if the command should succeed and 'false' if the command should fail
-# 4th parameter: the expected error message if the previous parameter is 'false'
-runSpark "$SPARK_USER1" "$command" "false" "$expectedErrorMsg"
+# 3rd parameter: 'shouldPass' if the command should succeed and 'shouldFail' if the command should fail
+# 4th parameter: the expected error message if the previous parameter is 'shouldFail'
+runSpark "$SPARK_USER1" "$command" "shouldFail" "$expectedErrorMsg"
---
+++ trino-hms-hdfs-ranger/big-data-tests/spark/test_10.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+source "./big-data-tests/lib.sh"
+source "./big-data-tests/env_variables.sh"
+
+set -e
+
+echo ""
+echo "## Test 10 ##"
+echo "Attempt various DDL operations against the managed table as a different user"
+echo ""
+
+# It's the same as in the previous test.
+updateHdfsPathPolicy "read,write,execute:$HDFS_USER,$SPARK_USER1/read,execute:$SPARK_USER2" "/$HIVE_WAREHOUSE_DIR/gross_test.db"
+
+updateHiveDbAllPolicy "alter,create,drop,index,lock,select,update:$SPARK_USER1/select:$SPARK_USER2" "gross_test"
+
+# It's the same as in the previous test.
+updateHiveDefaultDbPolicy "select:$SPARK_USER1,$SPARK_USER2"
+
+# It's the same as in the previous test.
+updateHiveUrlPolicy "read,write:$SPARK_USER1" "hdfs://$NAMENODE_NAME/$HIVE_WAREHOUSE_DIR/gross_test.db"
+
+# waitForPoliciesUpdate
+
+# Drop.
+command="spark.sql(\"drop table gross_test.test\")"
+
+expectedErrorMsg="Permission denied: user [$SPARK_USER2] does not have [DROP] privilege on [gross_test/test]"
+
+# 1st parameter: the user to execute the command
+# 2nd parameter: the command to be executed
+# 3rd parameter: 'shouldPass' if the command should succeed and 'shouldFail' if the command should fail
+# 4th parameter: the expected error message if the previous parameter is 'shouldFail'
+runSpark "$SPARK_USER2" "$command" "shouldFail" "$expectedErrorMsg"
+
+# Alter.
+command="spark.sql(\"alter table gross_test.test rename to gross_test.test2\")"
+
+expectedErrorMsg="Permission denied: user [$SPARK_USER2] does not have [ALTER] privilege on [gross_test/test]"
+
+runSpark "$SPARK_USER2" "$command" "shouldFail" "$expectedErrorMsg"
+
+# Create.
+command="spark.sql(\"create table gross_test.test2 (id int, greeting string)\")"
+
+expectedErrorMsg="Permission denied: user [$SPARK_USER2] does not have [CREATE] privilege on [gross_test/test2]"
+
+runSpark "$SPARK_USER2" "$command" "shouldFail" "$expectedErrorMsg"

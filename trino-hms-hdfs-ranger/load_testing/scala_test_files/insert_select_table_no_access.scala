133b105f06b5972745e3a144dfd44832b3b595f9
Christos Bisias <christosbis@gmail.com>
2024-06-26 19:56:23 +0300

    better error handling for insert_select_table_no_access.scala

--- trino-hms-hdfs-ranger/load_testing/scala_test_files/insert_select_table_no_access.scala
+++ trino-hms-hdfs-ranger/load_testing/scala_test_files/insert_select_table_no_access.scala
@@ -8,18 +8,34 @@ var counter = 0
 
 while (counter < iterations) {
 
+  // The user has 'select' access but doesn't have 'insert into' access.
+  // Therefore, we expect 'select' to succeed and 'insert into' to fail.
+
+  var insertIntoFailed = false
+
   try {
     spark.sql("insert into default.test2 values (" + counter + ", 'str" + counter + "')")
   } catch {
     case e: Exception =>
       println("Message: " + e.getMessage())
+      insertIntoFailed = true
   }
+
+  // If 'insert into' didn't fail as expected, then exit.
+  if (!insertIntoFailed) {
+    println("'insert into' was expected to fail but it didn't. Exiting...")
+    sys.exit(1)
+  }
+
+  val errorMsg = " failed during iteration '" + counter + "'"
 
   try {
     spark.sql("select * from default.test2").show()
   } catch {
     case e: Exception =>
+      println("'select table' " + errorMsg)
       println("Message: " + e.getMessage())
+      sys.exit(1)
   }
 
   println("--------------------------")

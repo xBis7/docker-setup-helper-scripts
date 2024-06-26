6643fbdeae5504eee33f7027ae490d4e748d8416
Christos Bisias <christosbis@gmail.com>
2024-06-26 22:44:28 +0300

    cleanup

--- trino-hms-hdfs-ranger/load_testing/env_variables.sh
+++ trino-hms-hdfs-ranger/load_testing/env_variables.sh
@@ -14,22 +14,8 @@ SPARK_USER3="test2"
 SPARK_USER4="test3"
 
 # Hostnames.
-NAMENODE_HOSTNAME="docker-namenode-1"
 DN1_HOSTNAME="docker-datanode-1"
-DN2_HOSTNAME="docker-datanode-2"
-DN3_HOSTNAME="docker-datanode-3"
-
-HMS_HOSTNAME="hive-metastore-ranger-hive-metastore-1"
-HMS_POSTGRES_HOSTNAME="hive-metastore-ranger-postgres-1"
-
-RANGER_HOSTNAME="ranger"
-RANGER_POSTGRES_HOSTNAME="ranger-postgres"
-RANGER_USERSYNC_HOSTNAME="ranger-usersync"
-
-TRINO_HOSTNAME="trino-coordinator-1"
-
 SPARK_MASTER_HOSTNAME="spark-master-1"
-SPARK_WORKER1_HOSTNAME="spark-worker-1"
 
 # Service Names.
 NAMENODE_NAME="namenode"
--- trino-hms-hdfs-ranger/load_testing/run_test.sh
+++ trino-hms-hdfs-ranger/load_testing/run_test.sh
@@ -15,47 +15,24 @@ test_num=$2
 iteration_num=$3
 background_run=$4
 
-run_test_file1=1
-run_test_file2=1
-run_test_file3=1
-run_test_file4=1
-
-# Check which test to run.
-if [[ "$test_num" == "1" ]]; then
-  run_test_file1=0
-elif [[ "$test_num" == "2" ]]; then
-  run_test_file2=0
-elif [[ "$test_num" == "3" ]]; then
-  run_test_file3=0
-elif [[ "$test_num" == "4" ]]; then
-  run_test_file4=0
-else
-  echo ""
-  echo "No test file has been selected to run. Exiting..."
-  exit 0
-fi
-
 # 'setup.sh' should have already copied the files.
 # Rerun the copy here so that we can apply changes without having to run 'setup.sh' again.
 copyTestFilesUnderSpark "$abs_path"
 
-if [ "$run_test_file1" == 0 ]; then
+if [ "$test_num" == "1" ]; then
   db_location="/opt/hive/data/gross_test/gross_test.db"
 
   runCreateDropDbOnRepeatWithAccess "$SPARK_USER1" "$iteration_num" "$db_location" "$background_run"
-fi
-
-if [ "$run_test_file2" == 0 ]; then
+elif [ "$test_num" == "2" ]; then
 
   runCreateDropTableOnRepeatWithAccess "$SPARK_USER2" "$iteration_num" "$background_run"
-fi
-
-if [ "$run_test_file3" == 0 ]; then
+elif [ "$test_num" == "3" ]; then
 
   runInsertSelectTableOnRepeatWithAccess "$SPARK_USER3" "$iteration_num" "$background_run"
-fi
-
-if [ "$run_test_file4" == 0 ]; then
+elif [ "$test_num" == "4" ]; then
 
   runInsertSelectTableOnRepeatNoAccess "$SPARK_USER4" "$iteration_num" "$background_run"
+else
+
+  echo "Invalid test number. Try one of the following: 1, 2, 3, 4."
 fi

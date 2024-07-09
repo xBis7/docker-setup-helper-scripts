65fef1d1521802221366f657ddd3b8f84c9ba15d
Christos Bisias <christosbis@gmail.com>
2024-07-09 20:21:13 +0300

    cleanup - delete testing files

--- trino-hms-hdfs-ranger/big-data-c3-tests/trino-spark-tests/test.sh
+++ trino-hms-hdfs-ranger/big-data-c3-tests/trino-spark-tests/test.sh
@@ -24,15 +24,9 @@ fi
 # createHdfsDir uses the '-p' option. If the directory already exists, there won't be an error.
 createHdfsDir "$HIVE_GROSS_DB_TEST_DIR"
 
-# ./big-data-c3-tests/trino-spark-tests/spark/test_spark.sh "$abs_path"
+./big-data-c3-tests/trino-spark-tests/spark/test_spark.sh "$abs_path"
 
 # # Cleanup any data leftovers from the spark tests.
-# ./big-data-c3-tests/trino-spark-tests/trino/cleanup.sh
+./big-data-c3-tests/trino-spark-tests/trino/cleanup.sh
 
-# ./big-data-c3-tests/trino-spark-tests/trino/test_trino.sh "$abs_path"
-
-# ./big-data-c3-tests/trino-spark-tests/trino/test_10_copy.sh "$prepare_env"
-
-./big-data-c3-tests/copy_files_under_spark.sh "$abs_path"
-
-./big-data-c3-tests/trino-spark-tests/spark/test_11_copy.sh "$prepare_env" "false" "true"
+./big-data-c3-tests/trino-spark-tests/trino/test_trino.sh "$abs_path"

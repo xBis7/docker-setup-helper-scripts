a56ee5b820a37e1d5c63cdcc2dd2cd184341d8b7
Christos Bisias <christosbis@gmail.com>
2024-07-26 17:07:12 +0300

    fix trino tests

--- trino-hms-hdfs-ranger/tests/trino/1_test_trino_no_hdfs_perms.sh
+++ trino-hms-hdfs-ranger/tests/trino/1_test_trino_no_hdfs_perms.sh
@@ -17,7 +17,7 @@ echo "- INFO: Trino user trino shouldn't be able to create a table without HDFS 
 echo "- INFO: All policies are to their defaults and Hive access to default DB has been removed for group public."
 echo ""
 
-command="create table hive.$DEFAULT_DB.$TRINO_TABLE (column1 varchar,column2 varchar) with (external_location = 'hdfs://namenode/$HDFS_DIR',format = 'CSV');"
+command="create table hive.$DEFAULT_DB.$TRINO_TABLE (id varchar, name varchar) with (external_location = 'hdfs://namenode/$HDFS_DIR',format = 'CSV');"
 # Failure due to lack of HDFS permissions.
 expectedMsg="Permission denied: user [trino] does not have [ALL] privilege on" # [hdfs://namenode:8020/$HDFS_DIR]"
 runTrino "trino" "$command" "shouldFail" "$expectedMsg"
